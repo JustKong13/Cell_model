dd4101be89dcf93cd45589f0a4ef2da85f1ef286
Justin Kong <64031655+JustKong13@users.noreply.github.com>
2021-06-29 00:22:27 -0400

    updated reactions for crn

--- added_reactions/crn.py
+++ added_reactions/crn.py
@@ -1,5 +1,7 @@
 # cartinine (crn) reactions
 
+# EC Number 1.14.11.1 https://metacyc.org/META/NEW-IMAGE?type=EC-NUMBER&object=EC-1.14.11.1 
+
 gbbtc = model.metabolites.get_by_id("gbbtn_c")
 akg = model.metabolites.get_by_id("akg_c")
 crn = model.metabolites.get_by_id("crn_c")
@@ -18,6 +20,26 @@ l_carnitine_biosynthesis.add_metabolites(
     }
 )
 
-model.add_reactions([l_carnitine_biosynthesis])
-cobra.io.save_json_model(model, "./models/iML1515_crn.json")
+# EC Number 1.3.8.13 https://biocyc.org/META/NEW-IMAGE?type=REACTION&object=CROBETREDUCT-RXN
+
+bbtcoa = model.metabolites.get_by_id("bbtcoa_c")
+nadph = model.metabolites.get_by_id("nadph_c")
+h = model.metabolites.get_by_id("h_c")
+ctbtcoa = model.metabolites.get_by_id("ctbtcoa_c")
+
+crobetreduct_rxn = Reaction("CROBETREDUCT-RXN")
+crobetreduct_rxn.name = "CROBETREDUCT RXN"
+crobetreduct_rxn.add_metabolites(
+    {
+        bbtcoa: -1.0,
+        nadph: -1.0,
+        h: -1.0,
+        nadph: 1.0,
+        ctbtcoa: 1.0,
+    }
+)
+
+model.add_reactions([l_carnitine_biosynthesis, crobetreduct_rxn])
 model.optimize().objective_value
+
+cobra.io.save_json_model(model, "./models/IMPROVED_iML1515.json")
